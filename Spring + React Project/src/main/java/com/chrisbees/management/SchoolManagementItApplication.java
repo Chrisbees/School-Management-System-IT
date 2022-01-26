099624d2aa5bb4c93c6de9c8e99e382899645975
Chrisbees <beesworld1@yahoo.com>
2022-01-26 22:35:26 +0100

    first commit

---
+++ Spring + React Project/src/main/java/com/chrisbees/management/SchoolManagementItApplication.java
@@ -0,0 +1,13 @@
+package com.chrisbees.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagementItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolManagementItApplication.class, args);
+    }
+
+}
